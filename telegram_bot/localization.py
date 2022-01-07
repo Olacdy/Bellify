6403a9d19151719caf2663b38661cf36712698e1
DBoFury <frikol3000@gmail.com>
2022-01-07 20:47:20 +0300

    Localization refactor

---
+++ telegram_bot/localization.py
@@ -0,0 +1,88 @@
+localization = {
+    'en': {
+        'add_command':
+        [
+            'Now send channel\'s URL.',
+            'Send Your custom channel name.',
+            ['New channel added with name', '. \nLast video is'],
+            'Unable to add a new channel, because one with the same name already exists. \nTry to come up with a new name or leave the name parameter empty.',
+            'This channel is already added to Your profile! \nLast video is',
+        ],
+        'remove_command':
+        [
+            'Select a channel that You would like to remove.',
+            'Sorry. There is no channels added right now, maybe try using /add command.',
+            'Your record was deleted successfully.',
+            'No channel with such name.',
+        ],
+        'check_command':
+        [
+            'Select a channel that You would like to check.',
+            'Sorry. There is no channels added right now, maybe try using /add command.',
+            'No channel with such name.',
+            'No new video on this channel. \nLast video is'
+        ],
+        'list_command':
+        [
+            'List of Your added channels',
+            'Sorry. There is no channels added right now, maybe try using /add command.',
+        ],
+        'help_command':
+        [
+            'Notification Bot manual.\n\nTo start type /add command with some YouTube channel URL.\nNow, if everything went smoothly🤞, You should have this channel in our database.\nTry to check whether it is true and type /list command.\nTo check if there is a new video on this channel try to use /check + name of the channel command.\nThis way You can get fresh information about the latest video from this channel,\nbut don`t worry You`ll be getting notifications automatically if a new video is out there.\nTo remove some channels, just type /remove + name of the channel command.\n\nNow You are free to add any channel from YouTube and this bot will take care of notifying You about new videos.\n\nTo contact developer follow this link: https://t.me/golovakanta'
+        ],
+        'lang_start_command':
+        [
+            'Please, select a language.',
+            'Thanks, You\'ll continue work on English.',
+        ],
+        'echo':
+        [
+            'Do You want to change channel\'s name?',
+            'This doesn\'t look like a URL 🤔. Try again.',
+        ]
+    },
+    'ru': {
+        'add_command':
+        [
+            'Теперь можете прислать URL канала.',
+            'Можете прислать имя, под которым хотите сохранить канал.',
+            ['Новый канал под именем', 'был добавлен.\nПоследнее видео'],
+            'Невозможно добавить новый канал под этим именем.\nПопробуйте придумать новое имя или оставить параметр имени пустым.',
+            'Этот канал уже добавлен к вашему профилю!\nПоследнее видео',
+        ],
+        'remove_command': [
+            'Выберите канал, который вы хотите удалить.',
+            'Извините, пока у вас нет никаких каналов, попробуйте добавить новый с помощью /add.',
+            'Ваш канал успешно удален.',
+            'Канала с таким именем не существует.',
+        ],
+        'check_command':
+        [
+            'Выберите канал, который вы хотите проверить.',
+            'Извините, пока у вас нет никаких каналов, попробуйте добавить новый с помощью /add.',
+            'Канала с таким именем не существует.',
+            'На этом канале еще нет нового видео. \nПоследнее видео'
+        ],
+        'list_command':
+        [
+            'Список добавленных вами каналов',
+            'Извините, пока у вас нет никаких каналов, попробуйте добавить новый с помощью /add.',
+
+        ],
+        'help_command':
+        [
+            'Notification Bot мануал.\n\nЧтобы начать пользоваться ботом, воспользуйтесь коммандой /add плюс ссылка на ютуб канал.\nТеперь, если ничего не сломалось🤞, этот канал будет добавлен в нашу базу данных.\nПопробуйте проверить так ли это и напишите комманду /list.\nЧтобы проверить появилось ли новое видео, воспользуйтесь коммандой /check плюс имя канала, которое было в списке.\nТак вы можете получать последнюю информацию касательно последнего видео на канале,\nоднако не волнуйтесь, вы все еще будете получать сообщения от этого бота автоматически, как только мы заметим новое видео на одном из добавленных вами каналов.\nДля того чтобы удалить какой-либо канал, используйте комманду /remove плюс имя канала.\n\nТеперь, когда вы знаете основные функциональности бота, вы можете добавлять любой интересующий вас канал, а бот позаботится о том, чтобы снабжать вас актуальной информацией относительно последнего видео на канале.\nДля связи с разработчиком: https://t.me/golovakanta'
+        ],
+        'lang_start_command':
+        [
+            'Пожалуйста, выберите язык.',
+            'Спасибо, теперь работа будет продолжена на русском.'
+        ],
+        'echo':
+        [
+            'Хотите ли вы изменить имя канала?',
+            'Что-то не похоже на URL 🤔. Попробуйте еще раз.',
+        ]
+    }
+}
