8b3e811c9ee214dcb67b5da194f9db54d18fc376
Oleg Didechkin <frikol3000@gmail.com>
2022-08-21 02:17:32 +0300

    Channel adding bug fixed

--- tests/test_livestream_notification.py
+++ tests/test_livestream_notification.py
@@ -35,7 +35,7 @@ def test_saved_livestream_notification_for_basic_user(basic_user, channel, video
         YouTubeVideo.objects.create(
             video_id=video_id,
             video_title=videos[video_id][0],
-            published_datetime=videos[video_id][1],
+            published_at=videos[video_id][1],
             channel=channel
         )
 
@@ -63,7 +63,7 @@ def test_first_saved_livestream_notification_for_premium_user(premium_user, chan
         YouTubeVideo.objects.create(
             video_id=video_id,
             video_title=videos[video_id][0],
-            published_datetime=videos[video_id][1],
+            published_at=videos[video_id][1],
             channel=channel
         )
 
@@ -92,7 +92,7 @@ def test_second_saved_livestream_if_channel_does_not_delete_its_livestreams_noti
         YouTubeVideo.objects.create(
             video_id=video_id,
             video_title=videos[video_id][0],
-            published_datetime=videos[video_id][1],
+            published_at=videos[video_id][1],
             channel=channel_deleted_livestreams_0
         )
 
@@ -117,7 +117,7 @@ def test_second_saved_livestream_if_channel_does_delete_its_livestreams_notifica
         YouTubeVideo.objects.create(
             video_id=video_id,
             video_title=videos[video_id][0],
-            published_datetime=videos[video_id][1],
+            published_at=videos[video_id][1],
             channel=channel_deleted_livestreams_1
         )
 
