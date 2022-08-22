e5434cc896bdd980e3e78a102941caddf3020f3e
Oleg Didechkin <frikol3000@gmail.com>
2022-08-22 23:28:15 +0300

    Improved checking for deleted livestreams task

--- bellify_bot/management/commands/check_for_deleted_livestreams.py
+++ bellify_bot/management/commands/check_for_deleted_livestreams.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.core.management import BaseCommand
 from django.utils.timezone import now
 from youtube.models import YouTubeChannel, YouTubeVideo
@@ -9,14 +10,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for channel in YouTubeChannel.objects.all():
-            for livestream in channel.livestreams.all():
-                if (now() - livestream.ended_at).total_seconds() > 3600 and not livestream.is_checked_for_deleted:
-                    livestream.set_checked()
+            for livestream in channel.livestreams.filter(ended_at__lte=now() - timedelta(hours=1), is_checked_for_deleted=False):
+                livestream.set_checked()
 
-                    saved_livestream = YouTubeVideo.objects.filter(
-                        channel=channel, video_id__exact=livestream.livestream_id).first()
+                saved_livestream = YouTubeVideo.objects.filter(
+                    channel=channel, video_id__exact=livestream.livestream_id).first()
 
-                    if not check_if_video_is_exists(livestream.livestream_url):
-                        livestream.delete()
-                        saved_livestream.delete() if saved_livestream else None
-                        channel.increment_deleted_livestreams()
+                if not check_if_video_is_exists(livestream.livestream_url):
+                    livestream.delete()
+                    saved_livestream.delete() if saved_livestream else None
+                    channel.increment_deleted_livestreams()
