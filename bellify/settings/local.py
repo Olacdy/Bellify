cc4dd0777f8309fdd5d71a7ae02b0a5cb69e972b
DBoFury <frikol3000@gmail.com>
2023-03-06 01:39:25 +0200

    Debug value from .env fixed wrong value key

--- bellify/settings/local.py
+++ bellify/settings/local.py
@@ -31,7 +31,7 @@ env.read_env(f'{BASE_DIR}/.env')
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('SECRET_KEY')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['*']  # env.list('ALLOWED_HOSTS')
 
